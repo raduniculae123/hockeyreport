86e100bc54a29b7cc307973c4489d71a2fdde131
raduniculae123 <raduniculae6624@gmail.com>
2023-02-02 16:19:21 +0000

    pdf creation

--- app/src/main/java/com/example/myapplication/MenuActivity.java
+++ app/src/main/java/com/example/myapplication/MenuActivity.java
@@ -1,11 +1,11 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -13,6 +13,8 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+
         Button newReportBtn = (Button) findViewById(R.id.reportBtn);
         newReportBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
--- app/src/main/java/com/example/myapplication/ReportActivity.java
+++ app/src/main/java/com/example/myapplication/ReportActivity.java
@@ -1,14 +1,32 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.SystemClock;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.FileOutputStream;
 
 public class ReportActivity extends AppCompatActivity {
 
@@ -20,6 +38,11 @@ public class ReportActivity extends AppCompatActivity {
 
     //Report Variables
     private int period = 1;
+
+
+    // constant code for runtime permissions
+    private static final int PERMISSION_REQUEST_CODE = 200;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +59,14 @@ public class ReportActivity extends AppCompatActivity {
         Button endShiftBtn = findViewById(R.id.endShiftBtn);
         Button startShiftBtn = findViewById(R.id.startShiftBtn);
         Button pauseShiftBtn = findViewById(R.id.pauseShiftBtn);
-        Button resetButton = findViewById(R.id.endShiftBtn);
         Button prevPeriodBtn = findViewById(R.id.prevPeriodBtn);
         Button nextPeriodBtn = findViewById(R.id.nextPeriodBtn);
+        Button endGameBtn = findViewById(R.id.endGameBtn);
 
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
 
         nextPeriodBtn.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
@@ -67,7 +94,7 @@ public class ReportActivity extends AppCompatActivity {
                 if (period <= 3) {
                     periodTxtView.setText("Period " + period);
 
-                } else if(period == 4){
+                } else if (period == 4) {
                     periodTxtView.setText("Period OT");
                 }
                 if (period == 1) {
@@ -99,7 +126,7 @@ public class ReportActivity extends AppCompatActivity {
             }
         });
 
-        resetButton.setOnClickListener(new View.OnClickListener() {
+        endShiftBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 chronometer.setBase(SystemClock.elapsedRealtime());
@@ -108,5 +135,44 @@ public class ReportActivity extends AppCompatActivity {
                 running = false;
             }
         });
+
+        endGameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    generatePDF();
+                }
+            }
+        });
+
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    private void generatePDF() {
+        Document document = new Document();
+        try {
+            String fileName = "pdf_file.pdf";
+
+            // Get the content resolver and create a new file
+            ContentResolver contentResolver = getContentResolver();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, fileName);
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "application/pdf");
+            contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS);
+            Uri uri = contentResolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, contentValues);
+
+            // Open the output stream and write the PDF file
+            FileOutputStream fos = (FileOutputStream) contentResolver.openOutputStream(uri);
+            PdfWriter.getInstance(document, fos);
+            document.open();
+            document.add(new Paragraph("Hello World!"));
+            document.close();
+
+            Log.d("PDF", "Report Created");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
